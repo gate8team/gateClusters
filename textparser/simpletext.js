39214f718bb9a818539945b8e8a802f12a8298f3
Anton Shabanov <me.antony.rain@gmail.com>
2014-06-03 15:11:35 +0400

    [ADDED]: added get words count functional. Fixed split words function.

--- textparser/simpletext.js
+++ textparser/simpletext.js
@@ -2,6 +2,7 @@ function SimpleText(title, text){
     this.title = title || '';
     this.text = text || '';
     this.wordsSplitted = [];
+    this.wordsCounted = [];
 };
 
 SimpleText.prototype.slitWords = function(removeEmptyWords){
@@ -19,6 +20,22 @@ SimpleText.prototype.slitWords = function(removeEmptyWords){
                 return str;
         });
     }
+
+    return this;
+};
+
+SimpleText.prototype.getWordsCount = function(){
+    if (this.wordsSplitted instanceof Array) {
+        var that = this;
+        this.wordsSplitted.forEach(function(el){
+            if (that.wordsCounted[el] != undefined) {
+                that.wordsCounted[el] += 1;
+            } else {
+                that.wordsCounted[el] = 1;
+            }
+        });
+    }
+    return this;
 };
 
 module.exports = SimpleText;
